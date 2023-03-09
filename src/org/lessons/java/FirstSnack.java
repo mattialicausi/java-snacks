a7cb6c8ba6ce9f7ceb43f338163a4ab2c49065ab
mattialicausi <licausivitomattia@gmail.com>
2023-03-09 14:08:49 +0100

    completato consegna primo snack in classe FirstSnack

--- src/org/lessons/java/FirstSnack.java
+++ src/org/lessons/java/FirstSnack.java
@@ -1,9 +1,21 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class FirstSnack {
     public static void main(String[] args) {
 
         /* Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.*/
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Inserisci un numero");
+        int n = Integer.parseInt(scan.nextLine());
+
+        if (n % 2 == 0) {
+            System.out.println("Il numero è pari: " + n);
+        } else {
+            System.out.println("Il numero è dispari: " + (n + 1));
+        }
 
     }
 }
