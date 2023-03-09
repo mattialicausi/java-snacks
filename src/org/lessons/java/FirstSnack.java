12a0db7a409cc05cfb529add23106d81f8b86037
mattialicausi <licausivitomattia@gmail.com>
2023-03-09 13:57:17 +0100

    scaffolding primo snack

---
+++ src/org/lessons/java/FirstSnack.java
@@ -0,0 +1,9 @@
+package org.lessons.java;
+
+public class FirstSnack {
+    public static void main(String[] args) {
+
+        /* Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.*/
+
+    }
+}
